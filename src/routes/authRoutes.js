a761fdde119e9df4f20023b6757da96c059b2989
amittahasilder <amittahasilder@gmail.com>
2026-09-24 02:46:53 -0700

    added a new file

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,30 @@
+
 const express = require("express");
 
-const { signup } = require("../controllers/authController");
+const {
+  signup,
+  login,
+  logout,
+} = require("../controllers/authController");
 
 const router = express.Router();
+
+// =====================================
+// AUTH ROUTES
+// =====================================
 
 // POST /api/auth/signup
 router.post("/signup", signup);
 
+// POST /api/auth/login
+router.post("/login", login);
+
+// POST /api/auth/logout
+router.post("/logout", logout);
+
+// =====================================
+// EXPORT ROUTER
+// =====================================
+
 module.exports = router;
+
